2bba99019c8b610cafc336acdeb3ec6051db76c3
Arda Soyturk <33841775+ardasoyturk@users.noreply.github.com>
2024-07-05 11:45:19 +0300

    use logger on BotProperties

--- src/main/java/com/emojibot/BotProperties.java
+++ src/main/java/com/emojibot/BotProperties.java
@@ -1,5 +1,8 @@
 package com.emojibot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -10,14 +13,15 @@ public class BotProperties {
 
     // Static execution block
     static {
+        Logger logger = LoggerFactory.getLogger("BotProperties");
         try (InputStream input = BotProperties.class.getClassLoader().getResourceAsStream("config.properties")) {
             if (input == null) {
-                System.out.println("Error! Unable to find config.properties");
+                logger.error("Unable to find config.properties");
             } else {
                 properties.load(input);
             }
         } catch (IOException ex) {
-            System.out.println(ex.toString());
+            logger.error(ex.toString());
         }
     }
 
